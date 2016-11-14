7d7de57dc0fda93486e0cb722580671412295ec7
Josue Orozco <jorozco@surgeforward.com>
2016-11-14 12:37:42 -0800

    Removing dist dir from accidental include

--- dist/5.c05c2ab81d51c941c461.chunk.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([5],{"./app/containers/NavbarContainer/sagas.js":function(e,r){"use strict";function t(){return regeneratorRuntime.wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.abrupt("return",!0);case 1:case"end":return e.stop()}},a[0],this)}Object.defineProperty(r,"__esModule",{value:!0}),r.defaultSaga=t;var a=[t].map(regeneratorRuntime.mark);r.default=[t]}});
