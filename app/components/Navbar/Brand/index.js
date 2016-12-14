feedb4f76ce8c22993b5cbcc728165ef52f0776c
Josue Orozco <jorozco@surgeforward.com>
2016-12-13 22:40:30 -0800

    Minor change to Brand text => Demo site.

--- app/components/Navbar/Brand/index.js
+++ app/components/Navbar/Brand/index.js
@@ -38,7 +38,7 @@ const Brand = () => (
                     'tagline',
                 )}
             >
-                Demo
+                Demo site.
             </span>
         </span>
     </Link>
