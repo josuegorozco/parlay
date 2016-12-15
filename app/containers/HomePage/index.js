b12d0cd86d18c4804926aee117c73e29c6b7937c
Josue Orozco <jorozco@billpaysolutions.com>
2016-12-15 11:36:26 -0800

    HomePage Update

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -11,31 +11,185 @@
 
 import React from 'react';
 import classNames from 'classnames';
-import FontAwesome from 'react-fontawesome';
 import styles from './styles.scss';
+import html5 from '../../assets/images/logos/html5.png';
+import css3 from '../../assets/images/logos/css3.png';
+import b4 from '../../assets/images/logos/bootstrap4.png';
+import sass from '../../assets/images/logos/sass.png';
+import react from '../../assets/images/logos/react.png';
+import redux from '../../assets/images/logos/redux.png';
+import nodejs from '../../assets/images/logos/nodejs.png';
+import npm from '../../assets/images/logos/npm.png';
+import express from '../../assets/images/logos/express.png';
+import webpack from '../../assets/images/logos/webpack.png';
+import mocha from '../../assets/images/logos/mocha.png';
+import chai from '../../assets/images/logos/chai.png';
+import karma from '../../assets/images/logos/karma.png';
+import js from '../../assets/images/logos/js.jpg';
+import eslint from '../../assets/images/logos/eslint.png';
+import babel from '../../assets/images/logos/babel.png';
 
 export default class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
     render() {
         return (
             <div className={classNames('container-fluid', styles.page)}>
-                <div className="jumbotron dker">
+                <div className="jumbotron">
                     <h1>
-                        <FontAwesome
-                            className={styles.icoHome}
-                            name="home"
-                        />
-                        Home Page
+                        React Boilerplate
                     </h1>
                     <p className="lead">
-                        This is a sample page.
+                        A highly scalable, offline-first foundation with the best DX and a focus on performance and best practices.
                     </p>
                     <hr />
                     <p>
-                        Sample paragraph here...
+                        Build on the latest tech stacks:
                     </p>
-                    <p className="lead">
-                        <button className="md-btn md-raised m-b-sm p-x success">Learn More</button>
+                    <div className="container-fluid">
+                        <div className="row v-s inline">
+                            <div className="col-xs">
+                                <div className="media">
+                                    <div className="media-middle">
+                                        <img src={b4} alt="Bootstrap 4" className="w-56" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xs">
+                                <div className="media">
+                                    <div className="media-middle">
+                                        <img src={react} alt="React" className="w-56" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xs">
+                                <div className="media">
+                                    <div className="media-middle">
+                                        <img src={redux} alt="Redux" className="w-56" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xs">
+                                <div className="media">
+                                    <div className="media-middle">
+                                        <img src={html5} alt="HTML5" className="w-56" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xs">
+                                <div className="media">
+                                    <div className="media-middle">
+                                        <img src={css3} alt="CSS 3" className="w-56" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xs">
+                                <div className="media">
+                                    <div className="media-middle">
+                                        <img src={sass} alt="SASS" className="w-56" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xs">
+                                <div className="media">
+                                    <div className="media-middle">
+                                        <img src={webpack} alt="Webpack" className="w-56" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xs">
+                                <div className="media">
+                                    <div className="media-middle">
+                                        <img src={js} alt="Javascript" className="w-56" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xs">
+                                <div className="media">
+                                    <div className="media-middle">
+                                        <img src={nodejs} alt="Nodejs" className="w-xs" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xs">
+                                <div className="media">
+                                    <div className="media-middle">
+                                        <img src={npm} alt="Nodejs" className="w-xs" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xs">
+                                <div className="media">
+                                    <div className="media-middle">
+                                        <img src={express} alt="Express" className="w-sm" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xs" />
+                        </div>
+                    </div>
+                    <p className="m-t-md">
+                        Deliver high quality code with:
                     </p>
+                    <div className="container-fluid">
+                        <div className="row v-s inline">
+                            <div className="col-xs">
+                                <div className="media">
+                                    <div className="media-middle">
+                                        <img src={eslint} alt="Express" className="w-56" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xs">
+                                <div className="media">
+                                    <div className="media-middle">
+                                        <img src={mocha} alt="Mocha" className="w-56" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xs">
+                                <div className="media">
+                                    <div className="media-middle">
+                                        <img src={chai} alt="Mocha" className="w-56" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xs">
+                                <div className="media">
+                                    <div className="media-middle">
+                                        <img src={karma} alt="Karma" className="w-sm" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xs">
+                                <div className="media">
+                                    <div className="media-middle">
+                                        <img src={babel} alt="Express" className="w-xs" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xs">
+                                <div className="media">
+                                    <div className="media-middle">
+                                        Sinon.JS
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xs">
+                                <div className="media">
+                                    <div className="media-middle">
+                                        expect
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xs">
+                                <div className="media">
+                                    <div className="media-middle">
+                                        enzyme
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-xs" />
+                        </div>
+                    </div>
                 </div>
             </div>
         );
